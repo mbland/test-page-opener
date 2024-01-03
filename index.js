777d31be728bcc6ad7d45e7568a750c4368630b3
Mike Bland <mbland@acm.org>
2024-01-03 15:33:38 -0500

    Update JSDoc comments, errors, s/load/open/g, etc.
    Lots of little changes to index.js that don't fundamentally alter
    existing behavior.

--- index.js
+++ index.js
@@ -6,6 +6,14 @@
  */
 
 /**
+ * Return value from PageOpener.open()
+ * @typedef OpenedPage
+ * @property {Window} window - Window object for the opened page
+ * @property {Document} document - Document object parsed from the opened page
+ * @property {Function} close - closes the page
+ */
+
+/**
  * Enables tests to open an application's own page URLs both in the browser and
  * in Node.js using jsdom.
  */
@@ -17,22 +25,21 @@ export class PageOpener {
 
   constructor(basePath) {
     if (!basePath.startsWith('/') || !basePath.endsWith('/')) {
-      const msg = 'basePath should start with \'/\' and ' +
-        'end with \'/\', got: '
-      throw new Error(`${msg}"${basePath}"`)
+      const msg = 'basePath should start with \'/\' and end with \'/\''
+      throw new Error(`${msg}, got: "${basePath}"`)
     }
     this.#basePath = basePath
     this.#opened = []
   }
 
-  async load(pagePath) {
+  async open(pagePath) {
     if (pagePath.startsWith('/')) {
-      const msg = 'page path should not start with \'/\', got: '
-      throw new Error(`${msg}"${pagePath}"`)
+      const msg = 'page path should not start with \'/\''
+      throw new Error(`${msg}, got: "${pagePath}"`)
     }
 
     const impl = await PageOpener.getImpl()
-    const page = await impl.load(this.#basePath, pagePath)
+    const page = await impl.open(this.#basePath, pagePath)
 
     this.#opened.push(page)
     return page
@@ -64,8 +71,8 @@ class BrowserPageOpener {
     this.#window = window
   }
 
-  // Loads a page and returns {window, document, close() } using the browser.
-  async load(basePath, pagePath) {
+  // Opens a page and returns {window, document, close()} using the browser.
+  async open(basePath, pagePath) {
     const w = this.#window.open(`${basePath}${pagePath}`)
     return new Promise(resolve => {
       const listener = () => {
@@ -91,14 +98,15 @@ class BrowserPageOpener {
   }
 }
 
-// Returns window and document objects from a JSDOM-parsed HTML file.
-//
-// It will execute <script type="module"> elements with a `src` attribute,
-// but not those with inline code. See the comment for
-// importModulesDynamically().
-//
-// Based on hints from:
-// - https://oliverjam.es/articles/frontend-testing-node-jsdom
+/**
+ * Returns window and document objects from a jsdom-parsed HTML file.
+ *
+ * It will import modules from `<script type="module">` elements with a `src`
+ * attribute, but not those with inline code.
+ *
+ * Based on hints from:
+ * - https://oliverjam.es/articles/frontend-testing-node-jsdom
+ */
 class JsdomPageOpener {
   #JSDOM
   #importModules
@@ -108,30 +116,37 @@ class JsdomPageOpener {
     this.#importModules = importModules
   }
 
-  // Loads a page and returns { window, document, close() } using JSDOM.
-  //
-  // For now, executes <script type="module"> elements with a `src` attribute,
-  // but does not execute such elements with inline code. This is because JSDOM
-  // currently parses, but doesn't execute, <script type="module"> elements:
-  //
-  // - https://github.com/jsdom/jsdom/issues/2475
-  //
-  // Once that issue is resolved, the explicit module loading behavior can
-  // be deleted from this implementation.
-  //
-  // Note on the timing of <script type="module"> execution
-  // ------------------------------------------------------
-  // Technically, the imported modules should execute similarly to
-  // <script defer> and execute before 'DOMContentLoaded'. However, modules
-  // imported with this function will execute _on_ 'DOMContentLoaded'. This is
-  // because the JSDOM 'DOMContentLoaded' and 'load' events will fire before the
-  // the dynamic import() calls resolve.
-  //
-  // This function sets a DOMContentLoaded listener that waits for the dynamic
-  // import() operations, and then manually fires DOMContentLoaded and load
-  // again. This enables (most) modules that register listeners for those
-  // events to behave as expected in JSDOM based tests.
-  async load(_, pagePath) {
+  /**
+   * Opens a page using jsdom.
+   *
+   * Imports modules from `<script type="module">` elements with a `src`
+   * attribute, but does not execute such elements with inline code. This is
+   * because jsdom currently parses, but doesn't execute,
+   * `<script type="module">` elements:
+   *
+   * - https://github.com/jsdom/jsdom/issues/2475
+   *
+   * Once that issue is resolved, the explicit module loading behavior can
+   * be deleted from this implementation.
+   *
+   * ### Note on the timing of `<script type="module">` execution
+   *
+   * Technically, the imported modules should execute similarly to
+   * `<script defer>` and execute before the "DOMContentLoaded" event.
+   * However, modules imported with this function will execute _on_ a simulated
+   * "DOMContentLoaded" event within a "load" event handler. This is because the
+   * original jsdom "DOMContentLoaded" and "load" events will fire before the
+   * the dynamic import() calls resolve.
+   *
+   * The "load" event listener waits for the dynamic `import()` operations, then
+   * manually fires the "DOMContentLoaded" and "load" again. This enables (most)
+   * modules that register listeners for those events to behave as expected in
+   * jsdom based tests.
+   * @param {string} _ - ignored
+   * @param {string} pagePath - path to the HTML file to load
+   * @returns {Promise<OpenedPage>} - object representing the opened page
+   */
+  async open(_, pagePath) {
     const { window } = await this.#JSDOM.fromFile(
       pagePath, {resources: 'usable', runScripts: 'dangerously'}
     )
@@ -246,18 +261,19 @@ class JsdomPageOpener {
 
   /**
    * Dynamically imports ECMAScript modules after the DOMContentLoaded event.
-   * @param {Window} window - the JSDOM window object
-   * @param {Document} document - the JSDOM window.document object
-   * @returns {Promise} - a Promise resolved after importing all ES modules
+   * @param {Window} window - the jsdom window object
+   * @param {Document} document - the jsdom window.document object
+   * @returns {Promise} - resolves after importing all ECMAScript modules
+   * @throws if importing any ECMAScript modules fails
    */
   #importModulesPromise(window, document) {
     return new Promise(resolve => {
-      const importModules = async () => {
-        // The JSDOM docs advise against setting global properties, but we don't
+      const importModulesOnEvent = async () => {
+        // The jsdom docs advise against setting global properties, but we don't
         // really have another option given any module may access window and/or
         // document.
         //
-        // (I tried to explore invoking ES modules properly inside the JSDOM,
+        // (I tried to explore invoking ES modules properly inside the jsdom,
         // and realized that way lies madness. At least, I couldn't yet figure
         // out how to access the Vite/Vitest module path resolver or Rollup
         // plugins. Then there's the matter of importmaps. I may still pick at
@@ -266,7 +282,7 @@ class JsdomPageOpener {
         // Also, unless the module takes care to close over window or document,
         // they may still reference the global.window and global.document
         // attributes. This isn't a common cause for concern in a browser, but
-        // resetting these global properties before a JSDOM listener fires can
+        // resetting these global properties before a jsdom listener fires can
         // cause it to error. This, in turn, can potentially cause a test to
         // hang or fail.
         //
@@ -278,13 +294,14 @@ class JsdomPageOpener {
         // listeners.
         //
         // The best defense against this problem would be to design the app to
-        // register closures over window and document. That would ensure they
-        // remain defined even after removal from globalThis.
+        // register closures over window and document, or specific document
+        // elements. That would ensure they remain defined even after we remove
+        // window and document from globalThis.
         globalThis.window = window
         globalThis.document = document
         await this.#importModules(window, document)
 
-        // The DOMContentLoaded and load events registered by JSDOM.fromFile()
+        // The DOMContentLoaded and load events registered by jsdom.fromFile()
         // will already have fired by this point.
         //
         // Manually firing DOMContentLoaded again after loading modules
@@ -297,11 +314,6 @@ class JsdomPageOpener {
         // For the same reason, we fire the 'load' event again as well. When
         // that listener executes, we can finally reset the global.window and
         // global.document variables.
-        const resetGlobals = () => {
-          delete globalThis.document
-          delete globalThis.window
-          resolve()
-        }
         document.dispatchEvent(new window.Event(
           'DOMContentLoaded', {bubbles: true, cancelable: false}
         ))
@@ -310,28 +322,28 @@ class JsdomPageOpener {
         // have fired. This attempts to ensure (but cannot guarantee) that the
         // global window and document objects remain valid for any 'load'
         // listeners registered by the DOMContentLoaded listeners.
+        const resetGlobals = () => resolve(
+          delete globalThis.document, delete globalThis.window
+        )
         window.addEventListener('load', resetGlobals, {once: true})
         window.dispatchEvent(new window.Event(
           'load', {bubbles: false, cancelable: false}
         ))
       }
-      document.addEventListener('DOMContentLoaded', importModules, {once: true})
+      document.addEventListener(
+        'DOMContentLoaded', importModulesOnEvent, {once: true}
+      )
     })
   }
 }
 
 /**
- * Imports <script type="module"> elements parsed, but not executed, by JSDOM.
+ * Imports modules from `<script type="module">` elements parsed by jsdom.
  *
- * Only works with scripts with a `src` attribute; it will not execute inline
- * code.
- *
- * If a module exports a default function(window, document), this will invoke
- * that function with the JSDOM window and document.
- * @private
- * @param {Window} window - the JSDOM window object
- * @param {Document} doc - the JSDOM window.document object
- * @returns {Promise} - a Promise resolved after importing all JS modules in doc
+ * Only works with the `src` attribute; it will not execute inline code.
+ * @param {Document} doc - the jsdom window.document object
+ * @returns {Promise} - resolves after importing all ECMAScript modules in doc
+ * @throws if importing any ECMAScript modules fails
  */
 function importModulesDynamically(window, doc) {
   const modules = doc.querySelectorAll('script[type="module"]')
