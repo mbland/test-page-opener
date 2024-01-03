7d0cfc07b2f6203a2677b2764d7dd673aef6a6c6
Mike Bland <mbland@acm.org>
2024-01-03 14:33:08 -0500

    Add Vite config, Vitest configs for CI
    The testing still isn't ready enough to turn the GitHub actions back on,
    but this is a step in that direction by allowing local testing.
    
    Note that it contains changes to run tests in GitHub Actions both with
    jsdom and in the browser (Chrome in CI). We'll see how well this works
    when we turn tests back on.

--- ci/vitest.config.js
+++ ci/vitest.config.js
@@ -1,12 +1,15 @@
 import { defineConfig, mergeConfig } from 'vitest/config'
-import baseConfig from '../vitest.config'
+import viteConfig from '../vite.config'
 
-export default mergeConfig(baseConfig, defineConfig({
+export default mergeConfig(viteConfig, defineConfig({
   test: {
+    outputFile: 'TESTS-TestSuites-jsdom.xml',
     reporters: [ 'junit', 'default' ],
     coverage: {
       enabled: true,
-      reporter: [ 'text', 'lcovonly' ]
+      provider: 'istanbul',
+      reporter: [ 'text', 'lcovonly' ],
+      reportsDirectory: 'coverage-jsdom'
     }
   }
 }))
---
+++ vite.config.js
@@ -0,0 +1,72 @@
+import { defineConfig } from 'vite'
+import { configDefaults } from 'vitest/config'
+import fs from 'node:fs'
+import os from 'node:os'
+
+const ARM64_LINUX_WARNING = [
+  '',
+  'WARNING:',
+  '-------',
+  'Neither Google Chrome nor Chrome for Testing are available for arm64 Linux.',
+  'Also, the snap versions of Chromium, Chromedriver, and Firefox are not',
+  'controllable by the webdriverio npm. For these reasons, you will need to',
+  'install non-snap versions of Chromium and/or Firefox to run the browser',
+  'tests.',
+  '',
+  'Some guidance for doing so on Ubuntu is available at:',
+  '- https://askubuntu.com/questions/1179273/how-to-remove-snap-completely-without-losing-the-chromium-browser/1206502#1206502',
+  '- https://www.omgubuntu.co.uk/2022/04/how-to-install-firefox-deb-apt-ubuntu-22-04',
+  '',
+  'Note that this may also require upgrading to at least Ubuntu 23.10, as it',
+  'will have more recent dependency updates that Chromium depends upon.',
+  ''
+]
+
+/**
+ * Configures browser tests to use Chromium on arm64 Linux
+ *
+ * Emits a warning and some suggestions if the system is arm64 Linux and
+ * /usr/bin/chromium is missing.
+ *
+ * Returns undefined if the system isn't arm64 Linux or if /usr/bin/chromium
+ * is missing.
+ * @returns {object | undefined} Chromium providerOptions or undefined
+ */
+function getProviderOptions(){
+  if (os.arch() !== 'arm64' || os.platform() !== 'linux') return
+
+  if (fs.existsSync('/usr/bin/chromium')) {
+    return {
+      capabilities: {
+        browserName: 'chromium',
+        'wdio:chromedriverOptions': {
+          binary: '/usr/bin/chromedriver'
+        },
+        'goog:chromeOptions': {
+          binary: '/usr/bin/chromium'
+        }
+      }
+    }
+  }
+  console.warn(ARM64_LINUX_WARNING.join('\n'))
+}
+
+export default defineConfig({
+  // Remove process.env.VITEST hack once the following are resolved/merged:
+  // - https://github.com/vitest-dev/vitest/issues/4686
+  // - https://github.com/vitest-dev/vitest/pull/4692
+  base: process.env.VITEST ? undefined : '/basedir/',
+  define: {
+    STRCALC_BACKEND: JSON.stringify(process.env.STRCALC_BACKEND)
+  },
+  test: {
+    coverage: {
+      reportsDirectory: 'coverage',
+      exclude: [...configDefaults.coverage.exclude, 'jsdoc', 'out']
+    },
+    browser: {
+      name: 'chrome',
+      providerOptions: getProviderOptions()
+    }
+  }
+})
