3711b4915ef6916eb5a0af9e381d951e700657af
Mike Bland <mbland@acm.org>
2024-01-04 13:46:57 -0500

    Add PageOpener tests
    PageOpener is now 100% covered. Tests uncovered parts of
    {Browser,JsDoc}PageOpener coming soon, with JSDoc and README updates to
    follow. Then we'll be ready to publish v1.0.0.

--- test/main.test.js
+++ test/main.test.js
@@ -13,12 +13,26 @@ describe('PageOpener', () => {
   beforeAll(async () => opener = await PageOpener.create('/basedir/'))
   afterEach(() => opener.closeAll())
 
-  test('contains the "Hello, World!" placeholder', async () => {
+  test('loads page with module successfully', async () => {
     const { document } = await opener.open('index.html')
     const appElem = document.querySelector('#app')
     const linkElem = document.querySelector('#app p a')
 
     expect(appElem.textContent).toContain('Hello, World!')
     expect(linkElem.href).toContain('%22Hello,_World!%22')
+  })
+
+  test('constructor throws if basePath is malformed', () => {
+    const newOpener = (basePath) => () => new PageOpener(basePath)
+    const  prefix = 'basePath should start with \'/\' and end with \'/\', got: '
+
+    expect(newOpener('basedir/')).toThrow(prefix + '"basedir/"')
+    expect(newOpener('/basedir')).toThrow(prefix + '"/basedir"')
+  })
+
+  test('open() throws if page path starts with \'/\'', async() => {
+    await expect(opener.open('/index.html')).rejects.toThrow(
+      'page path shouldn\'t start with \'/\', got: "/index.html"'
+    )
   })
 })
